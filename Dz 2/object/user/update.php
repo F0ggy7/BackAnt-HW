c77eba9fdb8301c230e79c1ae9ecd89c8bfa01d4
Bogdan <75094394+F0ggy7@users.noreply.github.com>
2023-03-01 02:48:26 +0300

    Add files via upload

---
+++ Dz 2/object/user/update.php
@@ -0,0 +1,26 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
+include_once '../config/database.php';
+include_once '../models/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+
+$user->id = $_POST['id'];
+$user->name = $_POST['name'];
+$user->city_id = $_POST['city_id'];
+$user->username = $_POST['username'];
+
+if ($user->update()) {
+    http_response_code(200);
+    echo json_encode(array("message" => "User has been updated."));
+} else {
+    http_response_code(503);
+    echo json_encode(array("message" => "Could't update user."));
+}
